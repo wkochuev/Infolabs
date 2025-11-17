aaf1d3f4db3ab92acafbf8db99885bb1ea8e6cc3
Max Kochuev <wkochuev@gmail.com>
2025-11-17 17:34:11 +0300

    Создание БД

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-
+import sqlite3;
+ 
+arendas_db = sqlite3.connect("arendas.db")
+cursor = arendas_db.cursor()
+ 
+# создаем таблицу people
+cursor.execute("""CREATE TABLE arendators
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                username TEXT)
+            """)
